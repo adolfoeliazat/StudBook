53f4036fddd1ab48907609d5f39729297b9e13aa
Andres Junge <ajunge@yaykuy.cl>
2015-08-14 21:51:42 -0400

    wip

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,5 +1,14 @@
-var welcome_string = "Hello from Truffle!";
-console.log(welcome_string);
-var addr = Example.deployed_address;
-console.log("Contract deployed to:"+addr);
+var accs= web3.eth.accounts;
+console.log("account:"+accs);
+
+var addr = Horse.deployed_address;
+console.log("Horse deployed to:"+addr);
+
+var horse=Horse.at(addr);
+
+horse.getOwner.call()
+.then (function(_ownerAddr){
+      console.log("ownerAddr:"+_ownerAddr);
+}).done();
+
 
