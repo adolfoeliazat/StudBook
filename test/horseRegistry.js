607abf4510cb810d72df21010a5fb2088f9c5687
Andres Junge <ajunge@yaykuy.cl>
2015-08-15 19:25:47 -0400

    test wip

--- test/horseRegistry.js
+++ test/horseRegistry.js
@@ -1,7 +1,35 @@
 contract('HorseRegistry', function(accounts) {
-  it("should assert true", function(done) {
-    var hr = HorseRegistry.at(HorseRegistry.deployed_address);
-    assert.isTrue(true);
-    done();
-  });
+
+var numHorses=0;
+var firstAddr=accounts[0];
+
+
+it("should add one horse", function(done) {
+	var horseRegistry = HorseRegistry.at(HorseRegistry.deployed_address);
+
+    horseRegistry.getNumHorses.call(firstAddr)
+	.then (function(_numHorses){
+	    numHorses=Number(_numHorses);
+	    console.log("  numHorses for "+firstAddr+" => "+_numHorses);
+	    return horseRegistry.newHorse("Gran Papa");
+	})
+	.then (function(o){
+		//console.log("  newHorse created! (tx:"+o+")");
+		return horseRegistry.getNumHorses.call(firstAddr);
+	})
+	.then (function(_numHorses){
+	    console.log("  numHorses for "+firstAddr+" => "+_numHorses);
+	    var expected = numHorses +1;
+	    assert.equal( _numHorses , expected);
+	})
+	.done(function(){
+	    assert.isTrue(true);
+		done();
+	});
+});
+
+
+
+
+
 });
